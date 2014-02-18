90768e62b836acbaacbad579712388d83291adca
itaylorm <taylor@imaxwell.net>
2014-02-17 18:06:36 -0800

    Updated Contact Grid

---
+++ UI/IMaxwell.UI.Mvc/Controllers/ContactController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IMaxwell.UI.Mvc.Views.Contact;
+using MvcPaging;
+
+namespace IMaxwell.UI.Mvc.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        public ActionResult Index(int? page)
+        {
+            var contacts = ContactService.GetContacts();
+            int currentPageIndex = page.HasValue ? page.Value - 1 : 0;
+            return View(contacts.ToPagedList(currentPageIndex, 15));
+        }
+
+        //
+        // GET: /Contact/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Contact/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Contact/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Contact/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Contact/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Contact/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Contact/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ UI/IMaxwell.UI.Mvc/Views/Contact/ContactService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace IMaxwell.UI.Mvc.Views.Contact
+{
+    public class ContactService
+    {
+        private const string BaseUri = "http://IMaxwell.net/Person/api/Contact/";
+
+        public static IEnumerable<Core.Model.Contact> GetContacts()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Task<string> response = httpClient.GetStringAsync(BaseUri);
+                var contacts = JsonConvert.DeserializeObjectAsync<IEnumerable<Core.Model.Contact>>(response.Result).Result;
+                return contacts;
+            }
+        }
+
+        public static IEnumerable<Core.Model.Contact> GetContacts(int start, int limit)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string request = String.Concat(BaseUri, String.Format("?start={0}&limit={1}", start, limit));
+
+
+                Task<string> response = httpClient.GetStringAsync(request);
+                var contacts = JsonConvert.DeserializeObjectAsync<IEnumerable<Core.Model.Contact>>(response.Result).Result;
+                return contacts;
+            }
+        }
+
+        public static Core.Model.Contact GetContact(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Task<string> response = httpClient.GetStringAsync(BaseUri + id);
+                return JsonConvert.DeserializeObjectAsync<Core.Model.Contact>(response.Result).Result;
+            }
+        }
+    }
+}
