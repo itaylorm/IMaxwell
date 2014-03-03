d345646844011f24f7ae1bcfad6875e8f1750dfa
itaylorm <taylor@imaxwell.net>
2014-03-03 09:53:16 -0800

    Added Employee Model Jasmine Tests

--- UI/IMaxwell.UI.ExtJs/spec/model/Contact.js
+++ UI/IMaxwell.UI.ExtJs/spec/model/Contact.js
@@ -4,8 +4,6 @@
 
         it('Instantiates without an issue', function () {
 
-            debugger;
-
             var contactModel = Ext.create('iMaxwell.model.Contact');
             expect(contactModel).toBeDefined();
             expect(contactModel instanceof iMaxwell.model.Contact).toBeTruthy();
---
+++ UI/IMaxwell.UI.ExtJs/spec/model/Employee.js
@@ -0,0 +1,14 @@
+﻿describe('iMaxwell.model.Employee', function () {
+
+    describe('Instantiation', function () {
+
+        it('Instantiates without an issue', function () {
+
+            var employeeModel = Ext.create('iMaxwell.model.Employee');
+            expect(employeeModel).toBeDefined();
+            expect(employeeModel instanceof iMaxwell.model.Employee).toBeTruthy();
+
+        });
+
+    });
+});
