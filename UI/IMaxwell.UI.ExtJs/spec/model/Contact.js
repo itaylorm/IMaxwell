fd14792316d880aa9b3141b2a8749d2c5d669885
itaylorm <taylor@imaxwell.net>
2014-03-03 09:33:21 -0800

    Added Jasmine 1.3 support with initial test for Contact Model

---
+++ UI/IMaxwell.UI.ExtJs/spec/model/Contact.js
@@ -0,0 +1,17 @@
+﻿describe('iMaxwell.model.Contact', function () {
+
+    describe('Instantiation', function() {
+
+        it('Instantiates without an issue', function () {
+
+            debugger;
+
+            var contactModel = Ext.create('iMaxwell.model.Contact');
+            expect(contactModel).toBeDefined();
+            expect(contactModel instanceof iMaxwell.model.Contact).toBeTruthy();
+
+        });
+    });
+
+
+});
