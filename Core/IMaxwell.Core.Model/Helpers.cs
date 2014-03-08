a84d2383675d61dff9e4f578d576e74f4e03b3b0
itaylorm <taylor@imaxwell.net>
2014-03-08 14:31:10 -0800

    Added Editable Currency Helper

--- Core/IMaxwell.Core.Model/Helpers.cs
+++ Core/IMaxwell.Core.Model/Helpers.cs
@@ -6,6 +6,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using IMaxwell.Core.Model;
 
 namespace IMaxwell.Core.Model
@@ -16,10 +17,10 @@ namespace IMaxwell.Core.Model
     /// </summary>
     public static class Helpers
     {
-        public static IHtmlString CurrencyDisplayFor<TModel, TProperty>(this HtmlHelper<TModel> helper,
-            Expression<Func<TModel, TProperty>> expression)
+        public static IHtmlString CurrencyFor<TModel, TProperty>(this HtmlHelper<TModel> helper,
+            Expression<Func<TModel, TProperty>> expression, object htmlAttributes)
         {
-            double value = double.Parse(expression.Compile().Invoke(helper.ViewData.Model).ToString());
+            var value = double.Parse(expression.Compile().Invoke(helper.ViewData.Model).ToString());
             var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
 
             var prop = typeof (TModel).GetProperty(metadata.PropertyName);
@@ -27,8 +28,12 @@ namespace IMaxwell.Core.Model
             var attribute = prop.GetCustomAttribute(typeof (CurrencyDisplayAttribute)) as CurrencyDisplayAttribute;
 
             // this should be whatever html element you want to create
-            TagBuilder tagBuilder = new TagBuilder("span");
-            tagBuilder.SetInnerText(value.ToString("c", CultureInfo.CreateSpecificCulture(attribute.Culture)));
+            var tagBuilder = new TagBuilder("input");
+
+            tagBuilder.Attributes.Add("type", "text");
+            tagBuilder.Attributes.Add("value", value.ToString("c", CultureInfo.CreateSpecificCulture(attribute.Culture)));
+
+            tagBuilder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
 
             return MvcHtmlString.Create(tagBuilder.ToString());
         }
